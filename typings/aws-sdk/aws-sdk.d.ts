3cfc72026447b535bb945bf052c4182184ebea6a
Edan Schwartz <eschwartz@weathernation.net>
2015-04-20 15:20:39 -0500

    Update aws typings with S3 methods

--- typings/aws-sdk/aws-sdk.d.ts
+++ typings/aws-sdk/aws-sdk.d.ts
@@ -1,8 +1,42 @@
+///<reference path="../definitely-typed/node/node.d.ts" />
+import fs = require('fs');
 declare module AWS {
+  interface Callback<TData> {
+    (err:Error, data:TData):void;
+  }
+
   class ElasticBeanstalk {
     constructor(...params:any[]);
     createApplication(...params:any[]):any;
     describeApplications(options:any, callback:{(err:string, data:any[])}):void;
+  }
+
+  class S3 {
+    public createBucket(params:any, cb?:Callback<any>);
+    public deleteBucket(params:any, cb?:Callback<any>);
+    public deleteObject(params:any, cb?:Callback<any>);
+    public getObject(params:any, cb?:Callback<any>);
+    public listBuckets(params:any, cb?:Callback<any>);
+    public listObjects(params:any, cb?:Callback<any>);
+    public putObject(params:S3.Params.putObject, cb?:Callback<S3.Response.putObject>);
+  }
+
+  module S3 {
+    module Params {
+      interface putObject {
+        Bucket: string;
+        Key: string;
+        Body: fs.ReadStream;
+      }
+    }
+
+    module Response {
+      interface putObject {
+        Expiration: string
+        ETag: string;
+        VersionId: string;
+      }
+    }
   }
 }
 
