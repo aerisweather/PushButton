90df171d0d7250a88fce00215369f7b6fa3cf2b1
Edan Schwartz <eschwartz@weathernation.net>
2015-04-20 11:32:16 -0500

    Add some typings for when.js

--- typings/vendor.d.ts
+++ typings/vendor.d.ts
@@ -1,4 +1,5 @@
 ///<reference path="./definitely-typed/tsd.d.ts" />
 ///<reference path="./aws-sdk/aws-sdk.d.ts" />
 ///<reference path="./wire/wire.d.ts" />
+///<reference path="./when/when.ext.d.ts" />
 ///<reference path="./util.d.ts" />
