7384400559adbdf96f497326c08fad0b181108c0
Edan Schwartz <eschwartz@weathernation.net>
2015-04-20 16:46:33 -0500

    GitArchiveProvider

--- scripts/objectDeploy.js
+++ scripts/objectDeploy.js
@@ -1,9 +1,9 @@
 var S3Object = require('../lib/resource/aws/s3/S3Object');
 var FileProvider = require('../lib/resource/system/file/FileProvider');
+var GitFileProvider = require('../lib/resource/system/file/GitArchiveProvider');
 var Bucket = require('../lib/resource/aws/s3/Bucket');
 
-var fileProvider = new FileProvider({
-  path: '/Users/edanschwartz/workspace/PushButton/README.md'
+var fileProvider = new GitFileProvider({
 });
 var bucket = new Bucket({
   name: 'turbine-eb'
@@ -11,20 +11,15 @@ var bucket = new Bucket({
 var s3Obj = new S3Object({
   fileProvider: fileProvider,
   bucket: bucket,
-  key: 'MyAwesomeFile'
+  key: 'GitArchive.zip'
 });
 
 s3Obj.deploy().
   then(function(res) {
     console.log(res.message);
+    process.exit(0);
   }).
   catch(function(err) {
     console.error(err);
+    process.exit(1);
   });
-
-
-
-
-function deploy() {
-
-}
