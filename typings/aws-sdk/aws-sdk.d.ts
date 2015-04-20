2f2c1bceda710f7ec76b3bb2819abf4f85a9c509
Edan Schwartz <eschwartz@weathernation.net>
2015-04-20 13:41:48 -0500

    whitespace

--- typings/aws-sdk/aws-sdk.d.ts
+++ typings/aws-sdk/aws-sdk.d.ts
@@ -1,11 +1,11 @@
 declare module AWS {
-    class ElasticBeanstalk {
-        constructor(...params:any[]);
-        createApplication(...params:any[]):any;
-        describeApplications(options:any, callback:{(err:string, data:any[])}):void;
-    }
+  class ElasticBeanstalk {
+    constructor(...params:any[]);
+    createApplication(...params:any[]):any;
+    describeApplications(options:any, callback:{(err:string, data:any[])}):void;
+  }
 }
 
 declare module 'aws-sdk' {
-    export = AWS;
+  export = AWS;
 }
