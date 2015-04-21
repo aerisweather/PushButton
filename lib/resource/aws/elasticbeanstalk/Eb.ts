d4520c996d46d2fb68161d6ca55b10f476ae00ea
Seth Miller <seth@four43.com>
2015-04-21 11:03:21 -0500

    Flushed out EB's deploy a little more, added some really dirty resource config checking.

--- lib/resource/aws/elasticbeanstalk/Eb.ts
+++ lib/resource/aws/elasticbeanstalk/Eb.ts
@@ -1,14 +1,13 @@
 /// <reference path="../../../../typings/vendor.d.ts" />
 
 import AWS = require('aws-sdk');
+import ConfigError = require('../../../error/ConfigError');
 import InvalidInputError = require('../../../error/InvalidInputError');
 import EbConfig = require('./EbConfig');
 import EbResult = require('./EbResult');
 import ResourceInterface = require('../../ResourceInterface');
 import when = require('when');
-import whenNodeLift = require('when/node');
-
-
+import lift = require('../../../util/lift');
 
 /**
  * A single deployable elastic beanstalk environment.
@@ -17,29 +16,30 @@ class ElasticBeanstalk implements ResourceInterface {
 
     resourceConfig:any;
     eb:AWS.ElasticBeanstalk;
+    createEbEnvironment:any;
 
     constructor(resourceConfig) {
         this.resourceConfig = resourceConfig;
+        if (this.resourceConfig.appVersion === undefined) {
+            throw new ConfigError('EB -> appVersion', 'An appVersion must be passed to an ElasticBeanstalk resource, this should reference an ElasticBeanstalk AppVersion resource.');
+        }
         this.eb = new AWS.ElasticBeanstalk({region: this.resourceConfig.region});
+        this.createEbEnvironment = lift<any>(AWS.ElasticBeanstalk.prototype.createApplication, this.eb);
     }
 
     public deploy():when.Promise<EbResult> {
 
         var ebConfig = new EbConfig(this.eb);
 
-        var parsedCreateConfig;
         return ebConfig.getEbCreateConfig(this.resourceConfig.config)
-            .tap(function (createConfig) {
-                //Application code already exists on S3, we have a reference to that S3 bucket
-
-                //Create appVersion
-
-            }.bind(this))
             .then(function (createConfig) {
-                return this.eb.createEnvironment(createConfig);
+                //AppVersion referenced by this.resourceConfig.appVersion
+                createConfig.VersionLabel = this.resourceConfig.appVersion.versionLabel;
+                return this.createEbEnvironment(createConfig);
+            })
+            .then(function (createEnvironmentResult) {
+                return new EbResult();
             });
-
-
     }
 
     public validateApplicationName(applicationName:string):when.Promise<boolean> {
