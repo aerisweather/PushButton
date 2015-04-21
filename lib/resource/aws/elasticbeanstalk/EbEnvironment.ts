a1753f5a9f2ffce1538ebc132034ef1239e8fdfc
Edan Schwartz <eschwartz@weathernation.net>
2015-04-21 11:47:32 -0500

    whitespace

--- lib/resource/aws/elasticbeanstalk/EbEnvironment.ts
+++ lib/resource/aws/elasticbeanstalk/EbEnvironment.ts
@@ -14,63 +14,62 @@ import lift = require('../../../util/lift');
  */
 class EbEnvironment implements ResourceInterface {
 
-    protected resourceConfig:any;
-    protected eb:AWS.ElasticBeanstalk;
-    protected createEbEnvironment:any;
+  protected resourceConfig:any;
+  protected eb:AWS.ElasticBeanstalk;
+  protected createEbEnvironment:any;
 
-    constructor(resourceConfig) {
-        this.resourceConfig = resourceConfig;
-        if (this.resourceConfig.appVersion === undefined) {
-            throw new ConfigError('EB -> appVersion', 'An appVersion must be passed to an ElasticBeanstalk resource, this should reference an ElasticBeanstalk AppVersion resource.');
+  constructor(resourceConfig) {
+    this.resourceConfig = resourceConfig;
+    if (this.resourceConfig.appVersion === undefined) {
+      throw new ConfigError('EB -> appVersion', 'An appVersion must be passed to an ElasticBeanstalk resource, this should reference an ElasticBeanstalk AppVersion resource.');
+    }
+    this.eb = new AWS.ElasticBeanstalk({region: this.resourceConfig.region});
+    this.createEbEnvironment = lift<any>(AWS.ElasticBeanstalk.prototype.createApplication, this.eb);
+  }
+
+  public deploy():when.Promise<EbResult> {
+    var ebConfig = new EbConfig(this.eb);
+
+    return ebConfig.getEbCreateConfig(this.resourceConfig.config)
+      .then(function (createConfig) {
+        //AppVersion referenced by this.resourceConfig.appVersion
+        createConfig.VersionLabel = this.resourceConfig.appVersion.versionLabel;
+        return this.createEbEnvironment(createConfig);
+      })
+      .then(function (createEnvironmentResult) {
+        return new EbResult();
+      });
+  }
+
+  public validateApplicationName(applicationName:string):when.Promise<boolean> {
+    return when.promise<boolean>(function (resolve, reject) {
+      this.eb.describeApplications({}, function (err, data) {
+        if (err) {
+          reject(err);
+          return false;
+        } // an error occurred
+        else {
+          if (data.Applications) {
+            var validApplications = data.Applications.filter(function (appData) {
+              return appData.ApplicationName === applicationName;
+            });
+            if (validApplications.length === 1) {
+              resolve(true);
+              return false;
+            }
+            else {
+              var availableApplications = data.Applications.map(function (appData) {
+                return appData.ApplicationName;
+              });
+
+              reject(new InvalidInputError('application', "Application was not found for this account. Try one of the following: \n\t\t" + availableApplications.join("\n\t\t")));
+            }
+          }
+          resolve(false);
         }
-        this.eb = new AWS.ElasticBeanstalk({region: this.resourceConfig.region});
-        this.createEbEnvironment = lift<any>(AWS.ElasticBeanstalk.prototype.createApplication, this.eb);
-    }
-
-    public deploy():when.Promise<EbResult> {
-
-        var ebConfig = new EbConfig(this.eb);
-
-        return ebConfig.getEbCreateConfig(this.resourceConfig.config)
-            .then(function (createConfig) {
-                //AppVersion referenced by this.resourceConfig.appVersion
-                createConfig.VersionLabel = this.resourceConfig.appVersion.versionLabel;
-                return this.createEbEnvironment(createConfig);
-            })
-            .then(function (createEnvironmentResult) {
-                return new EbResult();
-            });
-    }
-
-    public validateApplicationName(applicationName:string):when.Promise<boolean> {
-        return when.promise<boolean>(function (resolve, reject) {
-            this.eb.describeApplications({}, function (err, data) {
-                if (err) {
-                    reject(err);
-                    return false;
-                } // an error occurred
-                else {
-                    if (data.Applications) {
-                        var validApplications = data.Applications.filter(function (appData) {
-                            return appData.ApplicationName === applicationName;
-                        });
-                        if (validApplications.length === 1) {
-                            resolve(true);
-                            return false;
-                        }
-                        else {
-                            var availableApplications = data.Applications.map(function (appData) {
-                                return appData.ApplicationName;
-                            });
-
-                            reject(new InvalidInputError('application', "Application was not found for this account. Try one of the following: \n\t\t" + availableApplications.join("\n\t\t")));
-                        }
-                    }
-                    resolve(false);
-                }
-            });
-        });
-    }
+      });
+    });
+  }
 }
 
 export = EbEnvironment;
