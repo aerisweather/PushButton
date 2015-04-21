938af886a5c1b58dcf890c43afe043307bbc966d
Edan Schwartz <eschwartz@weathernation.net>
2015-04-21 14:52:16 -0500

    Convert sqsLifted to class
    - so we can instantiate with params

--- lib/resource/aws/sqs/service/SqsLifted.ts
+++ lib/resource/aws/sqs/service/SqsLifted.ts
@@ -3,15 +3,19 @@ import lift = require('../../../../util/lift');
 import AWS = require('aws-sdk');
 import SQS = AWS.SQS;
 
-interface SqsLifted {
-  createQueue(params:SQS.Params.createQueue):When.Promise<SQS.Response.createQueue>;
-  getQueueUrl(params:{ QueueName: string }):When.Promise<{ QueueUrl:string }>;
+class SqsLifted {
+  sqs: SQS;
+
+  createQueue:(params:SQS.Params.createQueue) => When.Promise<SQS.Response.createQueue>;
+  getQueueUrl:(params:{ QueueName: string }) => When.Promise<{ QueueUrl:string }>;
+
+  constructor(sqsParams = {}) {
+    this.sqs = new SQS(sqsParams);
+
+    this.createQueue = lift<SQS.Response.createQueue>(this.sqs.createQueue, this.sqs);
+    this.getQueueUrl = lift<{ QueueUrl: string}>(this.sqs.getQueueUrl, this.sqs);
+  }
+
 }
 
-var sqsProxy = new SQS();
-var sqsLifted:SqsLifted = {
-  createQueue: lift<SQS.Response.createQueue>(sqsProxy.createQueue, sqsProxy),
-  getQueueUrl: lift<{ QueueUrl: string}>(sqsProxy.getQueueUrl, sqsProxy)
-};
-
-export = sqsLifted;
+export = SqsLifted;
