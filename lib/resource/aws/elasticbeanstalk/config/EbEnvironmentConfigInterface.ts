c013aece88caf42a3d1f3207c1edc8cfe65041c3
Seth Miller <seth@four43.com>
2015-04-21 14:03:41 -0500

    Added some hints to EbEnvironmentConfigInterface.

--- lib/resource/aws/elasticbeanstalk/config/EbEnvironmentConfigInterface.ts
+++ lib/resource/aws/elasticbeanstalk/config/EbEnvironmentConfigInterface.ts
@@ -3,21 +3,30 @@ import ResourceConfigInterface = require('../../../config/ResourceConfigInterfac
 import EbAppVersion = require('../EbAppVersion');
 
 interface EbEnvironmentConfigInterface extends ResourceConfigInterface {
-  applicationName: string;
-  environmentName: string;
-  appVersion: EbAppVersion;
-  region: string;
-  solutionStack: {
-    os: string;
-    stack: string;
-  };
-  options?: any;
-  rawOptions?: any;
-  environmentVars?: Dictionary<string>;
-  description?: string;
-  cnamePrefix?: string;
-  tier?: string;
-  tags?: any;
-  templateName?: string;
+    applicationName: string;
+    environmentName: string;
+    appVersion: EbAppVersion;
+    region: string;
+    solutionStack: {
+        os: string;
+        stack: string;
+    };
+    options?: {
+        scaling?:any;
+        instances?:any;
+        notifications?:any;
+        updates?:any;
+        sqsWorker?:any;
+        loadBalancing?:any;
+        phpContainer?:any;
+        nodeJsContainer?:any;
+    };
+    rawOptions?: any;
+    environmentVars?: Dictionary<string>;
+    description?: string;
+    cnamePrefix?: string;
+    tier?: string;
+    tags?: any;
+    templateName?: string;
 }
 export = EbEnvironmentConfigInterface;
