12ec88dad920dca63c456bbcc3b980efe7d03011
Seth Miller <seth@four43.com>
2015-04-24 10:59:27 -0500

    Fixed bad merge conflict.

--- index.ts
+++ index.ts
@@ -18,141 +18,141 @@ import sequence = require('when/sequence');
  */
 
 interface PushButtonArg {
-  param: string;
-  description: string;
-  flag: string;
-  type: string;
+	param: string;
+	description: string;
+	flag: string;
+	type: string;
 }
 
 class PushButton {
-  protected cli:Admiral.Cli;
-  protected rl:readline.ReadLine;
-  protected config:any;
+	protected cli:Admiral.Cli;
+	protected rl:readline.ReadLine;
+	protected config:any;
 
-  public constructor() {
-    this.cli = new AdmiralCli({
-      helpOnNoArgs: false
-    });
-    this.rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-  }
+	public constructor () {
+		this.cli = new AdmiralCli({
+			helpOnNoArgs: false
+		});
+		this.rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
+	}
 
-  public run() {
-    return this.loadConfig().
-      tap((config) => this.config = config).
-      then((config) => this.parseArgs(config.args)).
-      then(() => this.deployResources());
-  }
+	public run () {
+		return this.loadConfig().
+			tap((config) => this.config = config).
+			then((config) => this.parseArgs(config.args)).
+			then(() => this.deployResources());
+	}
 
-  protected loadConfig():When.Promise<any> {
-    var configPath = process.argv.reduce((memo:any, arg:any) => {
-      return {
-        isNextArg: arg === '-c' || arg == '--config-path',
-        value: memo.isNextArg ? arg : memo.value
-      };
-    }, {})['value'];
+	protected loadConfig ():When.Promise<any> {
+		var configPath = process.argv.reduce((memo:any, arg:any) => {
+			return {
+				isNextArg: arg === '-c' || arg == '--config-path',
+				value: memo.isNextArg ? arg : memo.value
+			};
+		}, {})['value'];
 
-    // We have to tell Admiral CLI about the -c flag,
-    // even if we aren't using Admiral to grab it
-    // (otherwise it will complain)
-    this.cli.option('configPath', 'PushButton.json config file path', '-c', '--configPath', 'string');
+		// We have to tell Admiral CLI about the -c flag,
+		// even if we aren't using Admiral to grab it
+		// (otherwise it will complain)
+		this.cli.option('configPath', 'PushButton.json config file path', '-c', '--configPath', 'string');
 
-    if (configPath) {
-      return when(fs.readJsonSync(configPath));
-    }
-    return when(fs.readJsonSync(path.join(process.cwd(), 'PushButton.json')));
-  }
+		if (configPath) {
+			return when(fs.readJsonSync(configPath));
+		}
+		return when(fs.readJsonSync(path.join(process.cwd(), 'PushButton.json')));
+	}
 
-  protected parseCliOptions():When.Promise<any> {
-    return when.promise(
-      (resolve, reject) => {
-        try {
-          this.cli.parse();
-          resolve(this.cli.params);
-        }
-        catch (err) {
-          reject(err);
-        }
-      }).
-      catch(AdmiralCli.InvalidInputError, (err) => {
-        console.error(err);
-        process.exit(2);
-        return err;
-      }).
-      catch(AdmiralCli.ConfigError, (err) => {
-        console.error('Doh, configured something wrong.', err);
-        process.exit(1);
-        return err;
-      });
-  }
+	protected parseCliOptions ():When.Promise<any> {
+		return when.promise(
+			(resolve, reject) => {
+				try {
+					this.cli.parse();
+					resolve(this.cli.params);
+				}
+				catch (err) {
+					reject(err);
+				}
+			}).
+			catch(AdmiralCli.InvalidInputError, (err) => {
+				console.error(err);
+				process.exit(2);
+				return err;
+			}).
+			catch(AdmiralCli.ConfigError, (err) => {
+				console.error('Doh, configured something wrong.', err);
+				process.exit(1);
+				return err;
+			});
+	}
 
-  protected parseArgs(args:PushButtonArg[]) {
-    var mapAsync = <any>when['map'];
+	protected parseArgs (args:PushButtonArg[]) {
+		var mapAsync = <any>when['map'];
 
-	//Run!
-	rc.createResource().
-		done(quit, fail);
-});
+		// Setup options for each arg
+		args.forEach((arg:PushButtonArg) => {
+			this.cli.option(arg.param, arg.description, arg.flag, arg.flag, arg.type);
+		});
 
-    return this.parseCliOptions().
-      then((options:any) => {
-        return mapAsync(args, (arg:PushButtonArg) => {
-          return this.getArg(arg).
-            tap((argValue) => {
-              this.config.params[arg.param] = argValue;
-            });
-        });
-      });
-  }
+		return this.parseCliOptions().
+			then((options:any) => {
+				return mapAsync(args, (arg:PushButtonArg) => {
+					return this.getArg(arg).
+						tap((argValue) => {
+							this.config.params[arg.param] = argValue;
+						});
+				});
+			});
+	}
 
-  protected getArg(arg:PushButtonArg):When.Promise<any> {
-    // Try in cli params, first
-    if (this.cli.params[arg.param]) {
-      return when(this.cli.params[arg.param][0]);
-    }
+	protected getArg (arg:PushButtonArg):When.Promise<any> {
+		// Try in cli params, first
+		if (this.cli.params[arg.param]) {
+			return when(this.cli.params[arg.param][0]);
+		}
 
-    return this.ask(arg.description);
-  }
+		return this.ask(arg.description);
+	}
 
-  protected ask(question:string):When.Promise<any> {
-    return when.promise((resolve, reject) => {
-      this.rl.question(question + ': ', (answer:any) => {
-        resolve(answer)
-      });
-    });
-  }
+	protected ask (question:string):When.Promise<any> {
+		return when.promise((resolve, reject) => {
+			this.rl.question(question + ': ', (answer:any) => {
+				resolve(answer)
+			});
+		});
+	}
 
-  protected deployResources() {
-    var rc = new ResourceCollection(this.config);
+	protected deployResources () {
+		var rc = new ResourceCollection(this.config);
 
-    rc.on('deploy', function (result, resource) {
-      console.log(result.message);
-    });
+		rc.on('deploy', function (result, resource) {
+			console.log(result.message);
+		});
 
-    rc.on('error', function (error, resource) {
-      console.error(error);
-    });
+		rc.on('error', function (error, resource) {
+			console.error(error);
+		});
 
-    //Run!
-    rc.createResource().
-      done(quit, fail);
-  }
+		//Run!
+		rc.createResource().
+			done(quit, fail);
+	}
 }
 
 var pb = new PushButton();
 pb.run();
 
 
-function quit() {
-  console.log('Deployment successful');
-  process.exit(0);
+function quit () {
+	console.log('Deployment successful');
+	process.exit(0);
 }
 
-function fail(err) {
-  console.error(err);
-  process.exit(1);
+function fail (err) {
+	console.error(err);
+	process.exit(1);
 }
 
 
