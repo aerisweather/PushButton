98a6cda7fd715f3f5c0d41f99e2af7df7002ce5d
Edan Schwartz <eschwartz@weathernation.net>
2015-04-30 11:34:29 -0500

    Import params using `imports` config

--- lib/resource/ResourceCollection.ts
+++ lib/resource/ResourceCollection.ts
@@ -1,6 +1,7 @@
 /// <reference path="../../typings/vendor.d.ts" />
 import when = require('when');
 import sequence = require('when/sequence');
+import whenNode = require('when/node');
 import _ = require('lodash');
 import ResourceCollectionConfig = require('./../resource/config/ResourceCollectionConfigInterface');
 import Result = require('../resource/result/ResultInterface');
@@ -10,24 +11,51 @@ import ResourceServiceConfig = require('../config-manager/config/ResourceService
 import defaultResourceMap = require('../../config/resource-map');
 import ConfigManager = require('../config-manager/ConfigManager');
 import events = require('events');
+import fs = require('fs-extra');
+import path = require('path');
 
 
 class ResourceCollection extends events.EventEmitter implements Resource {
   protected config:ResourceCollectionConfig;
+  protected imports:ResourceCollectionConfig[];
 
   protected configManager:ConfigManager;
 
   constructor(config?:ResourceCollectionConfig) {
     super();
-    this.config = config;
+    this.config = this.processConfig(config || {});
 
     this.configManager = new ConfigManager();
     this.configManager.setResourceMap(defaultResourceMap);
   }
 
+  protected processConfig(config:ResourceCollectionConfig):ResourceCollectionConfig {
+    var readJsonSync = <any>fs.readJsonSync.bind(fs);         //  Fixes bad TS typing in fs.readJsonSync
+    var importPaths:string[] = config.imports || [];
+    var imports = importPaths.map<ResourceCollectionConfig>((relPath:String) => {
+      var absPath = path.join(process.cwd(), relPath);
+      return readJsonSync(absPath);
+    });
+
+    imports.forEach((importedConfig:ResourceCollectionConfig) => {
+      // extends params
+      config.params = _.extend(importedConfig.params, config.params);
+
+      // extend resources
+      importedConfig.resources.forEach((resourceConfig:ResourceServiceConfig) => {
+        var mainConfigHasResource = !!_.findWhere(config.resources, { name: resourceConfig.name });
+        if (!mainConfigHasResource) {
+          config.resources.push(resourceConfig);
+        }
+      });
+    });
+
+    return config;
+  }
+
   public createResource():when.Promise<ResourceCollectionResult> {
-    return this.configManager.
-      wireParams(this.config.params || {}).
+    return this.wireImports().
+      then(() => this.configManager.wireParams(this.config.params || {})).
       then(() => this.createAllServices()).
       then((results:Result[]) => {
         return {
@@ -35,6 +63,26 @@ class ResourceCollection extends events.EventEmitter implements Resource {
           results: results
         };
       });
+  }
+
+  protected wireParams() {
+    var readJsonSync = <any>fs.readJsonSync.bind(fs);         //  Fixes bad TS typing in fs.readJsonSync
+
+  }
+
+  protected wireImports() {
+    var readJsonSync = <any>fs.readJsonSync.bind(fs);         //  Fixes bad TS typing in fs.readJsonSync
+    var importPaths:string[] = this.config.imports || [];
+    var absolutePaths = importPaths.map<string>((relPath:string) => {
+      return path.join(process.cwd(), relPath);
+    });
+
+    return sequence(absolutePaths.map<When.PromiseFn<any>>((path:string) => {
+      return () => {
+        var config:ResourceCollectionConfig = readJsonSync(path);
+        return this.configManager.wireParams(config.params);
+      };
+    }));
   }
 
   protected createAllServices():When.Promise<Result[]> {
@@ -82,7 +130,7 @@ class ResourceCollection extends events.EventEmitter implements Resource {
   }
 
   public setConfig(config:ResourceCollectionConfig) {
-    this.config = config;
+    this.config = this.processConfig(config);
   }
 
   public getConfig():ResourceCollectionConfig {
