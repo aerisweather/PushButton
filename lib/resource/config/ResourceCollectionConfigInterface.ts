e46023f5485fb8e4ea7ea719dcaa9e7a579a9997
Edan Schwartz <eschwartz@weathernation.net>
2015-04-30 11:34:29 -0500

    Import params using `imports` config

--- lib/resource/config/ResourceCollectionConfigInterface.ts
+++ lib/resource/config/ResourceCollectionConfigInterface.ts
@@ -2,6 +2,8 @@ import ResourceConfigInterface = require('./ResourceConfigInterface');
 import ResourceServiceConfig = require('../../config-manager/config/ResourceServiceConfigInterface');
 
 interface ResourceCollectionConfigInterface {
+  /** Paths to other configs to import */
+  imports?:string[]
   params?:any;
   resources:ResourceServiceConfig[];
 }
