3b28f1258dbcd9bb091d1fff1f9951b442f06a41
Edan Schwartz <eschwartz@weathernation.net>
2015-05-20 13:52:43 -0500

    Logger

---
+++ lib/util/Logger.ts
@@ -0,0 +1,38 @@
+///<reference path="../../typings/vendor.d.ts" />
+import Events = require('events');
+
+class Logger {
+
+  public static TRACE = 'TRACE';
+  public static INFO = 'INFO';
+  public static WARNING = 'WARNING';
+  public static ERROR = 'ERROR';
+
+  protected static emitter = new Events.EventEmitter();
+
+  public static trace(message) {
+    Logger.log(message, Logger.TRACE);
+  }
+
+  public static info(message) {
+    Logger.log(message, Logger.INFO);
+  }
+
+  public static warning(message) {
+    Logger.log(message, Logger.WARNING);
+  }
+
+  public static error(message) {
+    Logger.log(message, Logger.ERROR);
+  }
+
+  public static log(message:string, level:string = Logger.INFO) {
+    Logger.emitter.emit(level, message);
+  }
+
+  public static handleLog(cb:() => void, level:string = Logger.INFO) {
+    Logger.emitter.on(level, cb);
+  }
+
+}
+export = Logger;
