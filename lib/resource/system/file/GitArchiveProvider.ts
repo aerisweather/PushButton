c7382511c1b5c7b986bf298c6a767ff3ee53fe5d
Edan Schwartz <eschwartz@weathernation.net>
2015-05-21 12:35:22 -0500

    minor: log GitArchive#createResource

--- lib/resource/system/file/GitArchiveProvider.ts
+++ lib/resource/system/file/GitArchiveProvider.ts
@@ -3,6 +3,7 @@ import FileProviderInterface = require('./FileProviderInterface');
 import FileProviderResult = require('./result/FileProviderResultInterface');
 import when = require('when');
 import childProcess = require('child_process');
+import Logger = require('../../../util/Logger');
 
 
 class GitArchiveProvider implements FileProviderInterface {
@@ -10,11 +11,17 @@ class GitArchiveProvider implements FileProviderInterface {
 
   public createResource():When.Promise<FileProviderResult> {
     return when.promise<FileProviderResult>((resolve:Function, reject:Function) => {
+      var archive;
+
+      Logger.trace('Creating git archive from ' + process.cwd());
+
       // Note that this will only archive the current working dir
       // More advanced git repo dir options should be implemented...
-      var archive = childProcess.execSync('git archive --format=zip HEAD', {
+      archive = childProcess.execSync('git archive --format=zip HEAD', {
         cwd: process.cwd()
       });
+
+      Logger.trace('Successfully created git archive.');
 
       resolve({
         message: 'Successfully archived git repo at ' + process.cwd(),
