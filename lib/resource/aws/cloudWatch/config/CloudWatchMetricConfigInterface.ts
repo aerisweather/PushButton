082477d4dcf5d2c31dff7c60ba205f6b4b19d1d2
Seth Miller <seth@four43.com>
2015-08-06 17:19:17 -0500

    Linked to some docs for cloudwatch

--- lib/resource/aws/cloudWatch/config/CloudWatchMetricConfigInterface.ts
+++ lib/resource/aws/cloudWatch/config/CloudWatchMetricConfigInterface.ts
@@ -1,17 +1,35 @@
 ///<reference path="../../../../../typings/vendor.d.ts" />
 import ResourceConfigInterface = require('../../../config/ResourceConfigInterface');
 
+/**
+ * CloudWatch Metric(Alarm) Config
+ *
+ * @see http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudWatch.html#putMetricAlarm-property
+ */
 interface CloudWatchMetricConfigInterface extends ResourceConfigInterface {
+
   name: string;
   description: string;
-  comparisonOperator: string;
-  evaluationPeriods: number;
-  metricName: string;
+
+  /**
+   * Namespaces:
+   * @see http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/aws-namespaces.html
+   */
   namespace: string;
+  /**
+   * Metric Names
+   * @see http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/CW_Support_For_AWS.html
+   */
+  metricName: string;
+
+  evaluationPeriods: number;
   period: number;
   statistic: string;
+
+  comparisonOperator: string;
   threshold: number;
   unit: string;
+
   actionsEnabled: boolean;
   alarmActions: string[];
   dimensions: any[];
